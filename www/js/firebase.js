218075cc174b4d88a43fc7dfce3ba9bb4a054b7e
Luis Henrique Moraes <luishmoraes@yahoo.com.br>
2021-09-14 15:05:03 -0300

    first commit

---
+++ www/js/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC7E7L0z3N7KpSSVvayh5JR_ZBpIzxC9C8",
+    authDomain: "cadastro-consulta.firebaseapp.com",
+    projectId: "cadastro-consulta",
+    storageBucket: "cadastro-consulta.appspot.com",
+    messagingSenderId: "898683450058",
+    appId: "1:898683450058:web:a2a89f96a208b5aff97a33",
+    measurementId: "G-35RB581Q66"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
